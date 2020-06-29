bc054f7ca42d3a06db2156fb9629d8394ef411a0
aidan_jabbarzadeh <jabbarz2@illinois.edu>
2020-06-29 13:35:51 -0500

    buildMarker works

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-//console.log("YO - index.js");
 const mapboxgl = require("mapbox-gl");
-const marker = require('./marker');
+const buildMarker = require('./marker');
 
 mapboxgl.accessToken = "pk.eyJ1IjoidHdlYXZlcjEiLCJhIjoiY2tjMG9sbjlyMTNrdDJ5cGo4Y2drZjdoMSJ9.PiPG_QbES5WqNLiebU2r7A";
 
-marker(-74.009, 40.705, 'hotel');
-
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v11" // mapbox has lots of different map styles available.
+  center: [-74.009, 40.705], // Fullstack NY coordinates
+  zoom: 12, 
+  style: "mapbox://styles/mapbox/streets-v11" // can be changed
 });
+
+const marker1 = buildMarker(-74.009, 40.705, 'hotel');
+const marker2 = buildMarker(-74.010, 40.7051, 'restaurant');
+const marker3 = buildMarker(-74.011, 40.7052, 'activity');
+
+marker1.addTo(map);
+marker2.addTo(map);
+marker3.addTo(map);
--- src/marker.js
+++ src/marker.js
@@ -1,19 +1,18 @@
 const mapboxgl = require("mapbox-gl");
 
-module.exports = function buildMarker(lad, long, type){
+module.exports = function buildMarker(long, lat, type){
     let markerDom = document.createElement('div');
     markerDom.style.width = "32px";
     markerDom.style.height = "39px";
    
-    if(type === "hotel"){
+    if (type === "hotel") {
         markerDom.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)";
-    }else if(type === "restaurant"){
+    } else if (type === "restaurant") {
         markerDom.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
-    }else{
+    } else if (type == "activity") {
         markerDom.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
     }
 
-
-    let marker = new mapboxgl.Marker(markerDom).setLngLat([lad, long]).addTo(map);
+    let marker = new mapboxgl.Marker(markerDom).setLngLat([long, lat]);
     return marker;
 }
